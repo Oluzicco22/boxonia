3253f719da78e09ba2acbf4bbc884e51dd79c013
Naythankik <dolapokiki3ird@gmail.com>
2025-07-29 19:36:26 +0100

    Fixed image responsiveness

--- src/components/ContactComponent/FAQArticle.jsx
+++ src/components/ContactComponent/FAQArticle.jsx
@@ -1,8 +1,9 @@
-import {useState} from "react";
-import {PiCaretDownThin, PiCaretUpThin} from "react-icons/pi";
+import { useState } from "react";
+import { PiCaretDownThin } from "react-icons/pi";
 
-const FAQArticle = ({articles}) => {
+const FAQArticle = ({ articles }) => {
     const [seeAnswer, setSeeAnswer] = useState(null);
+
     const toggleOptions = (id) => {
         setSeeAnswer(prevId => (prevId === id ? null : id));
     }
@@ -11,20 +12,27 @@ const FAQArticle = ({articles}) => {
         const isOpen = seeAnswer === index;
 
         return (
-            <button key={index} className="w-full bg-white rounded-lg p-5 shadow" onClick={() => toggleOptions(index)}>
+            <button
+                key={index}
+                className="w-full bg-white rounded-lg p-5 shadow text-left"
+                onClick={() => toggleOptions(index)}
+            >
                 <p className="flex items-center justify-between text-black font-semibold text-lg">
                     {article.question}
-                    {isOpen ?
-                        <PiCaretUpThin className="border-1 border-gray-900 w-6 h-6 rounded-full" /> :
-                        <PiCaretDownThin className="border-1 border-gray-900 w-6 h-6 rounded-full" />
-                    }
+                    <PiCaretDownThin
+                        className={`
+                            transition-transform duration-500 ease-in-out
+                            w-6 h-6 border border-gray-900 rounded-full
+                            ${isOpen ? '-rotate-180' : 'rotate-0'}
+                        `}
+                    />
                 </p>
-                <span className={`${isOpen ? 'inline-block' : 'hidden'} mt-4 text-left text-balance font-medium text-gray-500`}>
-                {article.answer}
-            </span>
+                <span className={`${isOpen ? 'inline-block' : 'hidden'} mt-4 text-left font-medium text-black border-t pt-6 border-gray-400 w-full`}>
+                    {article.answer}
+                </span>
             </button>
-        )
-    })
-}
+        );
+    });
+};
 
 export default FAQArticle;
