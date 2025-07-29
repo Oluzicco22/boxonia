f2e4589ba3448eaf23952dfed79b3f172e70606a
Naythankik <dolapokiki3ird@gmail.com>
2025-07-29 15:27:42 +0100

    Fixed image responsiveness

--- src/components/LandingPage/HeroSection.jsx
+++ src/components/LandingPage/HeroSection.jsx
@@ -2,8 +2,8 @@ const HeroSection = () => {
     return (
         <section className="h-[370px] flex w-[93%] mx-auto items-end">
             <div className="flex flex-col h-fit text-white hover:opacity-50">
-                <h1 className="orbitron text-xl md:text-9xl">action</h1>
-                <h1 className="orbitron text-xl md:text-9xl">everywhere</h1>
+                <h1 className="orbitron text-6xl md:text-9xl">action</h1>
+                <h1 className="orbitron text-6xl md:text-9xl">everywhere</h1>
             </div>
         </section>
     )
