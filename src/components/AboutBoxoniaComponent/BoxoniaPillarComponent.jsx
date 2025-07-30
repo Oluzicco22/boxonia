2b1848fee6e9bc3a03be7a3ab33c4a87e4b4f998
Isaac Olubusoye <isaacolubusoye@gmail.com>
2025-07-30 16:12:56 +0100

    more images added.

--- src/components/AboutBoxoniaComponent/BoxoniaPillarComponent.jsx
+++ src/components/AboutBoxoniaComponent/BoxoniaPillarComponent.jsx
@@ -1,16 +1,25 @@
 import boxoniaPillars from "../../data/our-work.json";
-import HeroImage from "../../assets/hero.png";
+import IdeationImage from "../../assets/ideation.svg";
+import End2EndImage from "../../assets/end2end-prod.svg";
+import MarketingImage from "../../assets/marketing.svg";
+import IpProtectionImage from "../../assets/ip-protection.svg";
 
 
 const BoxoniaPillarComponent = () => {
+    const images = [
+        IdeationImage,
+        End2EndImage,
+        MarketingImage,
+        IpProtectionImage
+    ]
     return (
         <section className="flex flex-col py-5 px-0 w-full mx-auto items-center">
             <div className="flex flex-col w-full md:gap-10">
                 {boxoniaPillars.map((pillar, idx) => (
                     <div key={idx}
-                         className="flex justify-center items-center py-32 w-full relative  bg-black/80 bg-blend-darken bg-cover bg-center"
+                         className="flex justify-center items-center py-32 w-full relative  bg-black/60 bg-blend-darken bg-cover bg-center"
                          style={{
-                             backgroundImage: `url(${HeroImage})`,
+                             backgroundImage: `url(${images[idx]})`,
                          }}
                          >
                         <div className="w-[92%] flex gap-3 text-white flex-col">
