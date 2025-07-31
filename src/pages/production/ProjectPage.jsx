0ebf00d8c38c8917516d6355e4a27d8c432b25d8
Isaac Olubusoye <isaacolubusoye@gmail.com>
2025-07-31 22:54:04 +0100

    project page fixed

--- src/pages/production/ProjectPage.jsx
+++ src/pages/production/ProjectPage.jsx
@@ -4,9 +4,15 @@ import Footer from "../../components/Footer.jsx";
 import {Link} from "react-router-dom";
 
 const ProjectPage = () => {
-    const images = [
-        "https://res.cloudinary.com/dybmufexj/image/upload/v1753986407/Nolly_babes_lnzopy.png",
-        "https://res.cloudinary.com/dybmufexj/image/upload/v1753986408/son_of_d_soil_vevinh.png"
+    const projects = [
+        {
+            image : "https://res.cloudinary.com/dybmufexj/image/upload/v1753986407/Nolly_babes_lnzopy.png",
+            status : true
+        },
+        {
+            image : "https://res.cloudinary.com/dybmufexj/image/upload/v1753986408/son_of_d_soil_vevinh.png",
+            status : false
+        }
         ]
     return (
         <>
@@ -14,14 +20,16 @@ const ProjectPage = () => {
             <div className="w-full flex justify-center my-10">
                 <ArticleHeader title="Projects" />
             </div>
-            {images.map((_, i) => (
+            {projects.map((project, i) => (
                 <article key={i}
                     className="my-10 flex justify-center items-end h-[40vh] w-full relative  bg-black/10 bg-blend-darken bg-cover bg-center"
                     style={{
-                        backgroundImage: `url(${images[i]})`,
+                        backgroundImage: `url(${project.image})`,
                     }}>
                     <div className="w-4/5 flex gap-3 text-white flex-col mb-16">
-                        <Link to={`/production/projects/${i + 1}`} className="text-right font-medium text-lg">{`see more --->`}</Link>
+                        {project.status ?
+                            <Link to={`/production/projects/${i + 1}`} className="text-right font-medium text-lg">{`see more --->`}</Link>
+                            : <p className="text-right font-medium text-lg"> coming soon...</p>}
                     </div>
                 </article>
             ))}
