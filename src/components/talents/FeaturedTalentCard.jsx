78caceba4fc7c1e0c721ca471f7a26e74c1253d5
Naythankik <dolapokiki3ird@gmail.com>
2025-08-03 15:17:29 +0100

    petty changes

--- src/components/talents/FeaturedTalentCard.jsx
+++ src/components/talents/FeaturedTalentCard.jsx
@@ -2,10 +2,10 @@ import {Link} from "react-router-dom";
 
 const FeaturedTalentCard = ({images}) => {
     return (
-        <div className="flex justify-between w-fit gap-10">
+        <div className="flex justify-between w-fit gap-3 md:gap-10">
             {images.map((image, i) => (
                 <Link to="#" key={i}>
-                    <img src={image} alt={`img-${i}`} className="w-28 md:w-[19.5rem]"></img>
+                    <img src={image} alt={`img-${i}`} className="w-full md:w-[19.5rem]"></img>
                 </Link>
             ))}
         </div>
