31beb37785df3bc42e6b725bf654058bf1a4c21e
Isaac Olubusoye <isaacolubusoye@gmail.com>
2025-10-22 08:04:39 +0100

    additional changes made on desktop screen only

--- src/components/production/InSectionLink.jsx
+++ src/components/production/InSectionLink.jsx
@@ -4,7 +4,7 @@ const InSectionLink = ({name, path, ariaLabel}) => {
         <a
             href={path}
            aria-label={ariaLabel}
-            className="text-center text-nowrap text-sm border-1 font-normal hover:bg-white hover:text-black border-[#989898] text-[#989898] px-10 md:px-20 py-3 mt-0 md:mt-8 rounded-2xl md:rounded-lg">{name}</a>
+            className="text-center text-nowrap text-sm border-1 font-normal hover:bg-white hover:text-black border-[#989898] text-[#989898] px-10 md:px-20 py-3 mt-0 md:mt-1 rounded-2xl md:rounded-lg">{name}</a>
     )
 }
 
